2c155cdfee66170d6eecb032d2e7786afc9ce59f
nwolfe <nikwolfe7@gmail.com>
2015-02-09 18:53:58 -0500

    stuff

--- src/Driver.java
+++ src/Driver.java
@@ -1,12 +1,28 @@
 import javax.sound.sampled.Mixer;
 
 public class Driver {
-
+  /**
+   * Entry to run the program
+   */
   public static void main(String[] args) {
-    System.out.println("Hello, world! " + AudioConstants.KHZ16.getValue());
-    Record record = new Record();
-    record.start();
-      
-  }
+      final Record recorder = new Record();
 
+      // creates a new thread that waits for a specified
+      // of time before stopping
+      Thread stopper = new Thread(new Runnable() {
+          public void run() {
+              try {
+                  Thread.sleep(Record.RECORD_TIME);
+              } catch (InterruptedException ex) {
+                  ex.printStackTrace();
+              }
+              recorder.finish();
+          }
+      });
+
+      stopper.start();
+
+      // start recording
+      recorder.start();
+  }
 }
--- src/Record.java
+++ src/Record.java
@@ -2,32 +2,59 @@ import java.io.*;
 import java.nio.ByteOrder;
 
 import javax.sound.sampled.*;
+import javax.sound.sampled.spi.FormatConversionProvider;
 
 public class Record {
 
-  public static final long RECORD_TIME = 5000;
+	public static final long RECORD_TIME = 5000;
 
-  File wavFile = new File("testaudio.wav");
+	File wavFile = new File("testaudio.wav");
 
-  private AudioFileFormat.Type fileType = AudioFileFormat.Type.WAVE;
+	private AudioFileFormat.Type fileType = AudioFileFormat.Type.WAVE;
 
-  private TargetDataLine line;
+	private TargetDataLine line;
 
-  public AudioFormat getAudioFormat() {
-    Float sampleRate = new Float(AudioConstants.KHZ16.getValue());
-    Integer sampleSizeInBits = 16;
-    Integer channels = AudioConstants.MONO.getValue();
-    Boolean signed = true;
-    Boolean bigEndian = (java.nio.ByteOrder.nativeOrder() == ByteOrder.BIG_ENDIAN);
-    AudioFormat format = new AudioFormat(sampleRate, sampleSizeInBits, channels, signed, bigEndian);
-    return format;
-  }
+	public AudioFormat getAudioFormat() {
+		Float sampleRate = new Float(AudioConstants.KHZ16.getValue());
+		Integer sampleSizeInBits = 16;
+		Integer channels = AudioConstants.MONO.getValue();
+		Boolean signed = true;
+		Boolean bigEndian = (java.nio.ByteOrder.nativeOrder() == ByteOrder.BIG_ENDIAN);
+		AudioFormat format = new AudioFormat(sampleRate, sampleSizeInBits,
+				channels, signed, bigEndian);
+		return format;
+	}
 
-  public void start() {
-    AudioFormat format = getAudioFormat();
-    DataLine.Info info  = new DataLine.Info(TargetDataLine.class, format);
-  }
+	public void start() {
+		try {
+			AudioFormat format = getAudioFormat();
+			DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
 
-  // WAV to output
+			if (!AudioSystem.isLineSupported(info)) {
+				System.out.println("Line not supported!");
+				System.exit(0);
+			}
+
+			System.out.println("Start capturing...");
+
+			AudioInputStream ais = new AudioInputStream(line);
+
+			System.out.println("Start recording");
+
+			// start recording
+			AudioSystem.write(ais, fileType, wavFile);
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	// WAV to output
+	public void finish() {
+		line.stop();
+		line.close();
+		System.out.println("Recording stoppped!");
+	}
 
 }
