277dd7704c67de1598dd591e67262d38144f4601
nikwolfe7 <nikwolfe7@gmail.com>
2015-02-10 20:57:31 -0500

    fixed...

--- src/Microphone.java
+++ src/Microphone.java
@@ -4,48 +4,40 @@ import javax.sound.sampled.Mixer.Info;
 
 public class Microphone {
 
-	private String preferredMicrophone = AudioStrings.ATR_USB_MICROPHONE.getValue();
-	private Mixer.Info foundMicrophone;
+  private String preferredMicrophone = AudioStrings.ATR_USB_MICROPHONE.getValue();
 
-	public Microphone() {
-		new Runnable() {
-			boolean found = false;
-			public void run() {
-				System.out.println("Looking for "+preferredMicrophone+"...");
-				while(!Thread.currentThread().isInterrupted()) {
-					for (Mixer.Info mixer : AudioSystem.getMixerInfo()) {
-						System.out.println("Attached mixer: " + mixer.getDescription());
-						if (mixer.toString().toLowerCase()
-								.contains(preferredMicrophone.toLowerCase())) {
-							found = true;
-							System.out.println("Found: " + preferredMicrophone + "!");
-							System.out.println("Creating connection to "
-									+ preferredMicrophone + "...");
-							
-							// Doing register stuff now...
-							
-							
-							
-							
-							
-							
-							
-							
-							Thread.currentThread().interrupt();
-							break;
-						} 
-					}
-					if(!found) {
-						try {
-							Thread.sleep(2000);
-							System.out.println(preferredMicrophone + " not found! Please find it... Don't worry, we'll wait.");
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					}
-				} // end thread while loop
-			} // end run method
-		}.run();
-	}
+  private Mixer.Info foundMicrophone;
+
+  public Microphone() {
+    new Runnable() {
+      boolean found = false;
+
+      public void run() {
+        System.out.println("Looking for " + preferredMicrophone + "...");
+        while (!Thread.currentThread().isInterrupted()) {
+          for (Mixer.Info mixer : AudioSystem.getMixerInfo()) {
+            if (mixer.getDescription().toLowerCase().contains(preferredMicrophone.toLowerCase())) {
+              found = true;
+              System.out.println("Found: " + preferredMicrophone + "!");
+              System.out.println("Creating connection to " + preferredMicrophone + "...");
+
+              // Doing register stuff now...
+
+              Thread.currentThread().interrupt();
+              break;
+            }
+          }
+          if (!found) {
+            try {
+              Thread.sleep(2000);
+              System.out.println(preferredMicrophone
+                      + " not found! Please find it... Don't worry, we'll wait.");
+            } catch (InterruptedException e) {
+              e.printStackTrace();
+            }
+          }
+        } // end thread while loop
+      } // end run method
+    }.run();
+  }
 }
