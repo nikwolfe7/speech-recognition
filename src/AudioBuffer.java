ced4a29d50171f965256a3e5168e390ee4ad7c4a
nwolfe <nikwolfe7@gmail.com>
2015-02-16 22:04:06 -0500

    asdfasdfsadasdfasdf

---
+++ src/AudioBuffer.java
@@ -0,0 +1,12 @@
+import java.io.ByteArrayOutputStream;
+
+
+public class AudioBuffer {
+
+  private ByteArrayOutputStream audioOutput;
+  
+  public AudioBuffer(Integer bufferSize) {
+    // TODO Auto-generated constructor stub
+  }
+
+}
