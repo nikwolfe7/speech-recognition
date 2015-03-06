f96f33d172b569e1dc91e6c9535ef95adda4a638
nikwolfe7 <nikwolfe7@gmail.com>
2015-03-05 21:53:49 -0500

    jgjh

--- src/mlsp/cs/cmu/edu/features/MFCCFeatureVectorContainer.java
+++ src/mlsp/cs/cmu/edu/features/MFCCFeatureVectorContainer.java
@@ -2,16 +2,17 @@ package mlsp.cs.cmu.edu.features;
 
 import java.util.ArrayList;
 
+import mlsp.cs.cmu.edu.audio.AudioConstants;
 import mlsp.cs.cmu.edu.segmentation.Segment;
 
 public class MFCCFeatureVectorContainer {
 
   private ArrayList<double[]> logMelSpectrum = new ArrayList<double[]>();
-  
+
   private ArrayList<double[]> MFCCs = new ArrayList<double[]>();
 
   private ArrayList<double[]> recoveredLogMelSpectrum = new ArrayList<double[]>();
-  
+
   private String segmentName;
 
   public MFCCFeatureVectorContainer(Segment seg) {
@@ -20,7 +21,26 @@ public class MFCCFeatureVectorContainer {
 
   // should only be called after all feature frames have been added
   public void expand() {
-    // TODO Auto-generated method stub
+    for (int t = 0; t < MFCCs.size(); t++) {
+      if (t > 2 && t < MFCCs.size()) {
+        double[] frame = MFCCs.get(t - 2);
+        double[] expandedFrame = new double[AudioConstants.MFCC_SIZE.getValue() * 3];
+        System.arraycopy(frame, 0, expandedFrame, 0, AudioConstants.MFCC_SIZE.getValue());
+        // deltas 
+        for (int n = AudioConstants.MFCC_SIZE.getValue(); n < expandedFrame.length - AudioConstants.MFCC_SIZE.getValue(); n++) {
+          double prev = MFCCs.get(t - 3)[n - AudioConstants.MFCC_SIZE.getValue()];
+          double next = MFCCs.get(t - 1)[n - AudioConstants.MFCC_SIZE.getValue()];
+          double delta = (next - prev) / 2;
+          expandedFrame[n] = delta;
+        }
+        MFCCs.set(t - 2, expandedFrame);
+        
+      }
+      double[] frame = MFCCs.get(t);
+      double[] expandedFrame = new double[AudioConstants.MFCC_SIZE.getValue() * 3];
+      System.arraycopy(frame, 0, expandedFrame, 0, AudioConstants.MFCC_SIZE.getValue());
+      MFCCs.set(t, expandedFrame);
+    }
   }
 
   public void accept(FeatureVisitor visitor) {
@@ -40,7 +60,7 @@ public class MFCCFeatureVectorContainer {
   }
 
   public void printMatlabScripts() {
-    
+
   }
 
 }
