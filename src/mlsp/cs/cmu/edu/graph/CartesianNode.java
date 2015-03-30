23655ec3ed3681c3a10b8278b636b3175318e1bd
nwolfe <nikwolfe7@gmail.com>
2015-03-30 19:49:44 -0400

    adfasfdasd

--- src/mlsp/cs/cmu/edu/graph/CartesianNode.java
+++ src/mlsp/cs/cmu/edu/graph/CartesianNode.java
@@ -1,9 +1,22 @@
 package mlsp.cs.cmu.edu.graph;
 
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
-public class CartesianNode extends Node {
+public class CartesianNode<T> {
+
+  private List<Node<T>> coordinates;
+  private List<CartesianNode<T>> successors; 
+  private Double score;
+
+  @SuppressWarnings("unchecked")
+  public CartesianNode(Node<T>... nodes) {
+    this.coordinates = new ArrayList<Node<T>>();
+    for(Node<T> node : nodes) {
+      coordinates.add(node);
+    }
+  }
   
-  private Map.Entry<Node, Node> coordinates;
+  
 
 }
--- src/mlsp/cs/cmu/edu/graph/Node.java
+++ src/mlsp/cs/cmu/edu/graph/Node.java
@@ -1,20 +1,68 @@
 package mlsp.cs.cmu.edu.graph;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class Node<T> {
   
   // the node's id is itself.
   private Node<T> id = this;
   
+  // the node's value
+  protected T value;
+  
   // score or cost
-  private Double score = 0.0;
+  protected Double score = 0.0;
   
-  // node and weight list
-  private List<Map.Entry<Node<T>, Double>> successors;
+  // node and edge weight list
+  protected List<Map.Entry<Node<T>, Double>> successors;
 
+  public Node(T value) {
+    this.value = value;
+    this.successors = new ArrayList<Map.Entry<Node<T>,Double>>();
+  }
   
-  
-  
+  public Node(T value, Double score) {
+    super();
+    this.value = value;
+    this.score = score;
+    this.successors = new ArrayList<Map.Entry<Node<T>,Double>>();
+  }
+
+  public Node(T value, Double score, List<Entry<Node<T>, Double>> successors) {
+    super();
+    this.value = value;
+    this.score = score;
+    this.successors = successors;
+  }
+
+  public Double getScore() {
+    return score;
+  }
+
+  public void setScore(Double score) {
+    this.score = score;
+  }
+
+  public List<Map.Entry<Node<T>, Double>> getSuccessors() {
+    return successors;
+  }
+
+  @SuppressWarnings("unchecked")
+  public void addSuccessors(Map.Entry<Node<T>, Double>... items) {
+    for(Map.Entry<Node<T>, Double> successor : items) {
+      this.successors.add(successor);
+    }
+  }
+
+  public Node<T> getId() {
+    return id;
+  }
+
+  public T getValue() {
+    return value;
+  }
+
 }
