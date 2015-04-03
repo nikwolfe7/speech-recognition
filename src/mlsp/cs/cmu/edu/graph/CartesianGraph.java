ebffcbf32fb79fe9de21ece404157b03088024d0
nwolfe <nikwolfe7@gmail.com>
2015-04-03 13:56:04 -0400

    asdfasdfasdfasd

--- src/mlsp/cs/cmu/edu/graph/CartesianGraph.java
+++ src/mlsp/cs/cmu/edu/graph/CartesianGraph.java
@@ -41,7 +41,7 @@ public abstract class CartesianGraph<N, E> {
   public Node<Coordinate<N, E>> getHeadNode() {
     return headNode;
   }
-  
+
   protected Coordinate<N, E> getCoordinate(Node<N> n1, Node<N> n2) {
     return new Coordinate<N, E>(n1, n2);
   }
@@ -53,41 +53,15 @@ public abstract class CartesianGraph<N, E> {
     headNode = getCoordinateNodeImpl(head);
     graphNodes.put(head, headNode);
 
-    Iterable<N> graph1 = G1.getNodes();
-    for (N g1Key : graph1) {
-      // for each node n1 in G1:
-      Node<N> n1 = G1.getNode(g1Key);
-
-      for (Edge<?> e1 : n1.getOutgoingEdges()) {
-        // for each node n2 in successors of (n1):
-        Node<?> n2 = e1.getNodePointer();
-
-        Iterable<N> graph2 = G2.getNodes();
-        for (N g2Key : graph2) {
-
-          // for each node n3 in G2:
-          Node<N> n3 = G2.getNode(g2Key);
-          for (Edge<?> e2 : n3.getOutgoingEdges()) {
-
-            // for each node n4 in successors of (n3):
-            Node<?> n4 = e2.getNodePointer();
-
-            // add edge n1,n3 --> n2,n4
-            Coordinate<N, E> c1 = getCoordinate(n1, n3);
-            Node<Coordinate<N, E>> node1 = getCoordinateNodeImpl(c1);
-
-            //Coordinate<N, E> c2 = getCoordinate(n2, n4);
-
-          }
-        }
-      }
-    }
+    // for each node n1 in G1:
+    // for each node n2 in successors of (n1):
+    // for each node n3 in G2:
+    // for each node n4 in successors of (n3):
+    // add edge n1,n3 --> n2,n4
 
   }
 
-  
-
   /* Defer node type construction to subclasses */
-  protected abstract Node<Coordinate<N, E>> getCoordinateNodeImpl(Coordinate<?,?> coordinate);
+  protected abstract Node<Coordinate<N, E>> getCoordinateNodeImpl(Coordinate<?, ?> coordinate);
 
 }
