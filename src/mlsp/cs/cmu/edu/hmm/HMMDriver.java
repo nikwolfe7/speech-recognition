558c9443572963b62432e21de1f3cc399c13c380
nikwolfe7 <nikwolfe7@gmail.com>
2015-04-13 08:16:51 -0400

    bvcvcbvg

--- src/mlsp/cs/cmu/edu/hmm/HMMDriver.java
+++ src/mlsp/cs/cmu/edu/hmm/HMMDriver.java
@@ -1,15 +1,21 @@
 package mlsp.cs.cmu.edu.hmm;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class HMMDriver {
 
   public static void main(String[] args) throws IOException {
+    
+    Files.deleteIfExists(new Path("./hw7-data/cleaned-hmm-train-japanese.txt"));
+    
     DataCleaner cleaner = new DataCleaner();
-    cleaner.cleanData("./hw7-data/hmm-train.txt", false);
+    cleaner.cleanData("./hw7-data/hmm-train.txt", true);
     cleaner.cleanData("./hw7-data/hmm-test.txt", false);
     cleaner.cleanData("./hw7-data/hmm-decode.txt", false);
-    cleaner.cleanData("./hw7-data/hmm-train-japanese.txt", true);
+    cleaner.cleanData("./hw7-data/hmm-train-japanese.txt", false);
     cleaner.cleanData("./hw7-data/hmm-test-japanese.txt", false);
     HMMFactory<String, Character> hmmFactory = CharacterHMMFactory.getInstance();
     HiddenMarkovModel<String, Character> HMM = hmmFactory.initializeHMM();
@@ -19,9 +25,11 @@ public class HMMDriver {
 //    HMM.A.getObservationProbability(HMM.Pi, HMM.B, "./hw7-data/toy-hmm-decode.txt");
 //    HMM.B.getObservationProbability(HMM.Pi, HMM.A, "./hw7-data/toy-hmm-decode.txt");
 
-    HMM.Viterbi.getViterbiBestPathFromFile("./hw7-data/toy-hmm-decode.txt");
-    HMM.Viterbi.getViterbiBestPathFromFile("./hw7-data/cleaned-hmm-decode.txt");
     HMM.Viterbi.getViterbiBestPathFromFile("./hw7-data/ex-hmm-decode.txt");
+    HMM.Viterbi.getViterbiBestPathFromFile("./hw7-data/cleaned-hmm-train-japanese.txt");
+    HMM.Viterbi.getViterbiBestPathFromFile("./hw7-data/cleaned-hmm-train.txt");
+    HMM.Viterbi.getViterbiBestPathFromFile("./hw7-data/cleaned-hmm-test.txt");
+    HMM.Viterbi.getViterbiBestPathFromFile("./hw7-data/cleaned-hmm-test-japanese.txt");
   
 //    HMM.trainHMMFromFile("./hw7-data/hmm-train-cleaned.txt");
     HMM.trainHMMFromFile("./hw7-data/cleaned-hmm-train-japanese.txt");
