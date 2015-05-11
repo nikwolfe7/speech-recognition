a18f6c9f2cf818ed2859f8393a272df0f038344b
nikwolfe7 <nikwolfe7@gmail.com>
2015-05-10 20:27:36 -0400

    llll

--- src/mlsp/cs/cmu/edu/graph/LexTree.java
+++ src/mlsp/cs/cmu/edu/graph/LexTree.java
@@ -7,51 +7,48 @@ import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
 
-public class LexTree<N, E> extends Graph<N, E> {
+public class LexTree<N, E> extends Graph<Character, String> {
+  
+  public LexTree(Node<Character> head) {
+    super(head);
+    // TODO Auto-generated constructor stub
+  }
 
-  public LexTree(Graph<N, E> graph) {
-    super(graph.getHeadNode());
-    setTailNode(graph.getTailNode());
-    ListIterator<Node<N>> nodeIter = getNodes().listIterator();
-    buildTree(getHeadNode());
-//    while(nodeIter.hasNext()) {
-//      buildTree(nodeIter.next());
+  private String[] processList;
+
+
+//  private void buildTree(Node<N> head) {
+//    ListIterator<Node<N>> nodeIter = head.getSuccessors().listIterator();
+//    Map<N, List<Node<N>>> map = new HashMap<N, List<Node<N>>>();
+//    while (nodeIter.hasNext()) {
+//      Node<N> node = nodeIter.next();
+//      if (!map.containsKey(node.getValue()))
+//        map.put(node.getValue(), new LinkedList<Node<N>>());
+//      map.get(node.getValue()).add(node);
 //    }
-  }
-
-  private void buildTree(Node<N> head) {
-    ListIterator<Node<N>> nodeIter = head.getSuccessors().listIterator();
-    Map<N, List<Node<N>>> map = new HashMap<N, List<Node<N>>>();
-    while (nodeIter.hasNext()) {
-      Node<N> node = nodeIter.next();
-      if (!map.containsKey(node.getValue()))
-        map.put(node.getValue(), new LinkedList<Node<N>>());
-      map.get(node.getValue()).add(node);
-    }
-    for (N key : map.keySet()) {
-      nodeIter = map.get(key).listIterator();
-      Node<N> first = nodeIter.next();
-      while (nodeIter.hasNext()) {
-        Node<N> second = nodeIter.next();
-        mergeNodes(first, second, head);
-        buildTree(first);
-      }
-    }
-  }
-
-  @SuppressWarnings("unchecked")
-  private void mergeNodes(Node<N> n1, Node<N> n2, Node<N> parent) {
-    ListIterator<Edge<?>> edgeIter = n2.getOutgoingEdges().listIterator();
-    while (edgeIter.hasNext()) {
-      Edge<?> e = edgeIter.next();
-      Node<N> successor = (Node<N>) e.getNodeSuccessor();
-      if (successor != n2)
-        e.setNodePredecessor(n1);
-      edgeIter.remove();
-    }
-    n1.refreshAll();
-    n2.destroy();
-    parent.refreshSuccessors();
-  }
+//    for (N key : map.keySet()) {
+//      nodeIter = map.get(key).listIterator();
+//      Node<N> first = nodeIter.next();
+//      while (nodeIter.hasNext()) {
+//        Node<N> second = nodeIter.next();
+//        mergeNodes(first, second, head);
+//      }
+//    }
+//  }
+//
+//  @SuppressWarnings("unchecked")
+//  private void mergeNodes(Node<N> n1, Node<N> n2, Node<N> parent) {
+//    ListIterator<Edge<?>> edgeIter = n2.getOutgoingEdges().listIterator();
+//    while (edgeIter.hasNext()) {
+//      Edge<?> e = edgeIter.next();
+//      Node<N> successor = (Node<N>) e.getNodeSuccessor();
+//      if (successor != n2)
+//        e.setNodePredecessor(n1);
+//      edgeIter.remove();
+//    }
+//    n1.refreshAll();
+//    n2.destroy();
+//    parent.refreshSuccessors();
+//  }
 
 }
--- src/mlsp/cs/cmu/edu/graph/LexTreeFactory.java
+++ src/mlsp/cs/cmu/edu/graph/LexTreeFactory.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import mlsp.cs.cmu.edu.spellchecker.CharNode;
+import mlsp.cs.cmu.edu.spellchecker.CharacterConstants;
 import mlsp.cs.cmu.edu.spellchecker.StringGraphFactory;
 
 public class LexTreeFactory implements GraphFactory<Character, String> {
@@ -14,20 +16,38 @@ public class LexTreeFactory implements GraphFactory<Character, String> {
   
   public LexTreeFactory(String... processList) {
     this.processList = processList;
-    List<String> stringSet = Arrays.asList(processList);
-    Set<String> arr = new HashSet<String>();
-    arr.addAll(stringSet);
-    processList = arr.toArray(new String[arr.size()]);
-    lexTree = new StringGraphFactory(processList).buildGraph();
+//    List<String> stringSet = Arrays.asList(processList);
+//    Set<String> arr = new HashSet<String>();
+//    arr.addAll(stringSet);
+//    processList = arr.toArray(new String[arr.size()]);
+//    lexTree = new StringGraphFactory(processList).buildGraph();
   }
   
-  public LexTreeFactory(Graph<Character, String> graph) {
-    this.lexTree = graph;
-  }
+//  public LexTreeFactory(Graph<Character, String> graph) {
+//    this.lexTree = graph;
+//  }
 
   @Override
   public Graph<Character, String> buildGraph() {
-    return new LexTree<Character, String>(lexTree);
+    Node<Character> head = new CharNode(CharacterConstants.BEGIN_CHARACTER.getValue());
+    Node<Character> tail = new CharNode(CharacterConstants.END_CHARACTER.getValue());
+    Graph<Character, String> G = new Graph<Character, String>(head);
+    G.addNode(tail); // tie the tail back to the head.
+    G.setTailNode(tail);
+    for (String stringGraph : processList) {
+      for (Character c : stringGraph.toCharArray()) {
+        Node<Character> newNode = new CharNode(c);
+        for(Node<Character> node : G.getHeadNode().getSuccessors()) {
+          if(node.getValue() == newNode.getValue())
+        }
+        
+      }
+    }
+    return G;
+  }
+  
+  private void trace(Character[] arr) {
+    
   }
 
 }
